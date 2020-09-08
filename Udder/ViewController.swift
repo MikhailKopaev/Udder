5eb92732b4d452bba6d0a1e4a34da7b28fa293d2
Mikhail Kopaev <mikhailkopaev@gmail.com>
2020-09-08 14:54:31 +0500

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Mikhail Kopaev on 08.09.2020.
+//  Copyright © 2020 Mikhail Kopaev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
